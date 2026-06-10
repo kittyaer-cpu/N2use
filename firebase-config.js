e8f89158c74399686394edaf680a2e1755ee80ef
kittyaer-cpu <kittyaer@kimo.com>
2026-06-10 15:19:26 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDn5RZa-O2nvtzbo5jlD5GmDr-IsnrK1_I",
+  authDomain: "n2use-9a2d6.firebaseapp.com",
+  projectId: "n2use-9a2d6",
+  storageBucket: "n2use-9a2d6.firebasestorage.app",
+  messagingSenderId: "891462862642",
+  appId: "1:891462862642:web:7d6ca090bae6689762f92b"
+};
